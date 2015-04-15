77c724600d424fc1505a7cb93ec96c0491cd3d4c
David Beniamine <david.beniamine@imag.fr>
2015-04-15 18:51:06 -0300

    Experiment: matrix

---
+++ code/is.c
@@ -0,0 +1,23 @@
+/*  Now, buckets are sorted.  We only need to sort keys inside
+    each bucket, which can be done in parallel.  Because the distribution
+    of the number of keys in the buckets is Gaussian, the use of
+    a dynamic schedule should improve load balance, thus, performance     */
+#ifdef SCHED_CYCLIC
+|\label{lst:is-cyclic}|#pragma omp for schedule(static,1)
+#else
+#pragma omp for schedule(dynamic)
+#endif
+for( i=0; i< NUM_BUCKETS; i++ ) {
+  k1 = i * num_bucket_keys;
+  k2 = k1 + num_bucket_keys;
+  for ( k = k1; k < k2; k++ )
+    key_buff1[k] = 0;
+
+  |\label{lst:is-gaus}| m = (i > 0)? bucket_ptrs[i-1] : 0;
+  for ( k = m; k < bucket_ptrs[i]; k++ )
+    key_buff1[key_buff2[k]]++; |\label{lst:is-gaus-end}|
+
+  key_buff1[k1] += m;
+  for ( k = k1+1; k < k2; k++ )
+    key_buff1[k] += key_buff1[k-1];
+}
